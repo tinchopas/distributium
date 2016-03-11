3defb490c117960e1427406b0e6266da5cda9c73
mpascal <mspascal@gmail.com>
2016-03-11 20:28:55 -0300

    Quiz results added

--- src/Distributium/FrontendBundle/Controller/QuizController.php
+++ src/Distributium/FrontendBundle/Controller/QuizController.php
@@ -27,4 +27,20 @@ class QuizController extends Controller
             'lodgingTypes' => $lodgingTypes
 	    );    
     }
+
+    /**
+     * @Route("/results", options={"expose"=true})
+     * @Template()
+     */
+    public function resultsAction()
+    {
+        $lodgingTypes = $this->getDoctrine()->getRepository('DistributiumBackendBundle:LodgingType')->findAll();
+
+	    return array(
+            'lodgingSizes' => $this->container->getParameter('lodgingSize'),
+            'lodgingCategories' => $this->container->getParameter('lodgingCategory'),
+            'lodgingRegions' => $this->container->getParameter('lodgingRegion'),
+            'lodgingTypes' => $lodgingTypes
+	    );    
+    }
 }
